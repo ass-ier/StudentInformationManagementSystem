680930f47347ef921022131bd8c8088ab25a9688
112233445566778899000 <32573960+112233445566778899000@users.noreply.github.com>
2022-04-28 17:42:22 +0300

    Initial Commit

---
+++ CS222_Final_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS222_Final_Project
+//
+//  Created by Assier Anteneh Alemu on 28/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
